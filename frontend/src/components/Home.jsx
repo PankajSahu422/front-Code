8daf282ce5a793a5dcc00c4cf64786d82eb5028e
PankajSahu02 <sahupankaj01423@gmail.com>
2025-08-09 23:25:45 +0530

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
-      home
+      <HeroSection/>
+      <CategoryCarousel/>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import persistStore from 'redux-persist/es/persistStore'
+import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
+const persistor = persistStore(store);
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
     <App />
+  
+      {/* </PersistGate> */}
+    </Provider>
   </StrictMode>,
 )
